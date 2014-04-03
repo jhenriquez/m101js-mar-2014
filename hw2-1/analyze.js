c05606e331e36fb2a5af2b300783459dd9997b7d
unknown <jhenriquez@DR-Developer.racoindustries.com>
2014-04-03 16:22:29 -0400

    Added Homework 2.1 (solved)

---
+++ hw2-1/analyze.js
@@ -0,0 +1,25 @@
+// MongoDB for NodeJS Developer Homework Week 2, Assignment 1
+
+var client = require('mongodb').MongoClient;
+
+client.connect('mongodb://localhost:27017/m101', function(err, db) {
+	if(err) {
+	  console.log(err.message);
+	  return;
+	}
+
+	var query = {'Wind Direction' : { $gt : 180, $lt : 360 } };
+	var projection = { 'State' : 1, Temperature : 1, 'Wind Direction' : 1,_id : 0 };
+	var options = { sort : { Temperature : 1 }, limit : 1 };
+
+	db.collection('hw2_1').find(query, projection, options).each(function(err, doc) {
+		if(err) {
+			console.log(err.message);
+			return db.close();
+		}
+
+		if(!doc) return db.close();
+
+		console.dir(doc.State);
+	});
+});
