a6f37b508adb461c358c3d9839864a83a8f04a2e
Julio Henriquez <julio.m.henriquez@gmail.com>
2014-04-08 23:17:02 -0400

    HW3.1 First Steps.

--- hw3-1/app.js
+++ hw3-1/app.js
@@ -3,5 +3,16 @@ var mongo = require('mongodb').MongoClient;
 mongo.connect('mongondb://localhost:27017/school', function(err, db) {
 	if(err) throw err;
 	
-	
+	db.collection('students').find().toArray(function(err, students) {
+		if(err) throw err;
+
+		students.forEach(function(student) {
+			var grades = student.grades;
+			var lowest = grades.shift();
+			for(i = 0; i < grades.length; i++) {
+				if(lowest.score > grades[i])
+					lowest = grades[i];
+			}
+		});
+	});
 });
